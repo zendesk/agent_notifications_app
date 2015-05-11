7f5b9eea6463be12e1e26cd8b6b144a1f9cac466
Ben Evans <bevans@zendesk.com>
2015-05-11 11:09:30 -0500

    basic message aggregation

--- push/app.js
+++ push/app.js
@@ -5,13 +5,36 @@
       'app.activated':'doSomething',
       'app.deactivated':'doSomething',
       '*.changed':'doSomething',
-      'ticket.submit.done':function() {this.doSomething('ticket.submit.done')},
-      'ticket.save':function() {this.doSomething('ticket.save'); return true;},
+      'ticket.submit.done':function() {this.doSomething(this.generateEvent('ticket.submit.done'));},
+      'ticket.save':function() {this.doSomething(this.generateEvent('ticket.save')); return true;},
       'ticket.submit.always':'doSomething',
     },
 
+    maxUpdateRate: 5000,
+    eventUpdates: [],
+    waiting: false,
+
+    generateEvent(e) {
+        return {'propertyName': e, 'timestamp': Date.now()};
+    }
+
+    aggregateEvent: function(e) {
+        if(!this.waiting) {
+            this.waiting = true;
+            setTimeout(_.bind(this.postData, this), this.maxUpdateRate);
+        }
+        this.eventUpdates.push(e);
+    },
+
     doSomething: function(e) {
-        console.log(e);
+        this.aggregateEvent(e);
+    },
+
+    postData: function() {
+        var updates = this.eventUpdates;
+        this.eventUpdates = [];
+        this.waiting = false;
+        console.log(updates);
     }
   };
 
