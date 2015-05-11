e53438dd4afe8c52a30383d09ce08f34aa71c4d5
Ben Evans <bevans@zendesk.com>
2015-05-11 17:10:21 -0500

    switched to tabs, added sample conditions

--- agent_notification/app.js
+++ agent_notification/app.js
@@ -1,32 +1,44 @@
 (function() {
 
   return {
-    events: {
-      'app.activated':'doSomething'
-    },
+	events: {
+	  'app.activated':'doSomething'
+	},
 
-    message: {
-        header:  'hello',
-        content: '<p>world</p>',
-        confirm: 'confirm',
-        cancel:  'cancel',
-        options: 'options',
-    },
+	message: {
+		header:  'hello',
+		content: '<p>world</p>',
+		confirm: 'confirm',
+		cancel:  'cancel',
+		options: 'options',
+	},
 
-   /* conditions {
-        all: ["field": "update_type",
-"operator": "is",
-"value": "Create"]
-    },*/
+	conditions {
+		all: [{
+				"field": "assignee_id",
+				"operator": "is_not",
+				"value": "current_user"
+			},
+			{
+				"field": "status",
+				"operator": "is_not",
+				"value": "solved"
+			}],
+		any: [{
+				"field": "status",
+				"operator": "is_not",
+				"value": "closed"
+		}]
+	},
 
-    doSomething: function() {
-        this.require = require('context_loader')(this);
-        this.require('popmodal')(this.message, _.bind(this.hideApp, this), _.bind(this.hideApp, this));
-    },
+	doSomething: function() {
+		this.require = require('context_loader')(this);
+		this.require('popmodal')(this.message, _.bind(this.hideApp, this), _.bind(this.hideApp, this));
+	},
 
-    hideApp: function() {
-        this.hide();
-    }
+	hideApp: function() {
+		this.hide();
+	}
   };
 
 }());
