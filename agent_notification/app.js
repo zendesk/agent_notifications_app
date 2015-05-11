4d5c7e7c9fb27148aac03510eeacab14458bf4d4
Ben Evans <bevans@zendesk.com>
2015-05-11 16:42:35 -0500

    working modal and app hiding

--- agent_notification/app.js
+++ agent_notification/app.js
@@ -5,7 +5,20 @@
       'app.activated':'doSomething'
     },
 
+    message: {
+        header:  'hello',
+        content: '<p>world</p>',
+        confirm: 'confirm',
+        cancel:  'cancel',
+        options: 'options',
+    },
+
     doSomething: function() {
+        this.require = require('context_loader')(this);
+        this.require('popmodal')(this.message, _.bind(this.hideApp, this), _.bind(this.hideApp, this));
+    },
+
+    hideApp: function() {
         this.hide();
     }
   };
