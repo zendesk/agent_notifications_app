ed6adef8a849cb671a68e258ce8500372912ed14
Ben Evans <bevans@zendesk.com>
2015-05-11 10:48:14 -0500

    added event handlers for many options

--- push/app.js
+++ push/app.js
@@ -2,10 +2,16 @@
 
   return {
     events: {
-      'app.activated':'doSomething'
+      'app.activated':'doSomething',
+      'app.deactivated':'doSomething',
+      '*.changed':'doSomething',
+      'ticket.submit.done':function() {this.doSomething('ticket.submit.done')},
+      'ticket.save':function() {this.doSomething('ticket.save'); return true;},
+      'ticket.submit.always':'doSomething',
     },
 
-    doSomething: function() {
+    doSomething: function(e) {
+        console.log(e);
     }
   };
 
