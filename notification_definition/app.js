6f204ce8b61d0f34fa3e8ea9af3a680780c766a8
Dan Pawluk <dpawluk@zendesk.com>
2015-05-11 20:10:58 -0500

    fleshed out functionality for autocomplete

--- notification_definition/app.js
+++ notification_definition/app.js
@@ -1,11 +1,85 @@
 (function() {
 
   return {
+  	requests: {
+      autocompleteRequester: function(name) {
+        return {
+          url: helpers.fmt('/api/v2/users/autocomplete.json?name=%@', name),
+          type: 'GET'
+        };
+      },
+      autocompleteOrganization: function(name) {
+        return {
+          url: helpers.fmt('/api/v2/organizations/autocomplete.json?name=%@', name),
+          type: 'GET'
+        };
+      }
+  	},
+
     events: {
-      'app.activated':'doSomething'
+      'app.activated':'doSomething',
+      'change .chosen_dropdown': 'show_operation'
     },
 
     doSomething: function() {
+      this.autocompleteRequesterName();
+      this.autocompleteOrganizationName();
+    },
+
+    show_operation: function(e) {
+    	e.preventDefault();
+      var current_option = e.currentTarget.value;
+
+    },
+
+    autocompleteRequesterName: function() {
+      var self = this;
+      this.$('#user_auto').autocomplete({
+        minLength: 3,
+        source: function(request, response){
+          self.ajax('autocompleteRequester', request.term).done(function(data){
+            response(_.map(data.users, function(user) {
+              return {
+                "label": user.name,
+                "value": user.id
+              };
+            }));
+          });
+        },
+        select: function(event, ui) {
+          event.preventDefault();
+          self.$('#user_auto').val(ui.item.label);
+        },
+        focus: function(event, ui) {
+          event.preventDefault();
+          self.$('#user_auto').val(ui.item.label);
+        }
+      }, this);
+    },
+
+    autocompleteOrganizationName: function() {
+      var self = this;
+      this.$('#org_auto').autocomplete({
+        minLength: 3,
+        source: function(request, response){
+          self.ajax('autocompleteOrganization', request.term).done(function(data){
+            response(_.map(data.organizations, function(org) {
+              return {
+                "label": org.name,
+                "value": org.id
+              };
+            }));
+          });
+        },
+        select: function(event, ui) {
+          event.preventDefault();
+          self.$('#org_auto').val(ui.item.label);
+        },
+        focus: function(event, ui) {
+          event.preventDefault();
+          self.$('#org_auto').val(ui.item.label);
+        }
+      }, this);
     }
   };
 
