5375b2e3669ef559785fe70c95d0fd420b240482
Craig Day <cday@zendesk.com>
2015-05-11 20:23:12 -0500

    basic index

--- notification_definition/app.js
+++ notification_definition/app.js
@@ -2,11 +2,35 @@
 
   return {
     events: {
-      'app.activated':'doSomething'
+      'app.activated':'index',
+      'click #newNotification': 'newNotification'
     },
 
-    doSomething: function() {
-    }
+    index: function() {
+      var notifications = {
+        active: this.activeNotifications,
+        inactive: this.inactiveNotifications
+      };
+
+      this.switchTo('index', notifications);
+    },
+
+    newNotification: function(e) {
+      e.preventDefault();
+      this.switchTo('new_notification', null);
+    },
+
+    activeNotifications: [
+      {id: 1, name: "Here", otherStuff: {}},
+      {id: 2, name: "Hello", otherStuff: {}},
+      {id: 2, name: "Nope", otherStuff: {}},
+      {id: 2, name: "Face Punch", otherStuff: {}}
+    ],
+
+    inactiveNotifications: [
+      {id: 1, name: "I'm inactive", otherStuff: {}},
+      {id: 2, name: "No this is boring", otherStuff: {}}
+    ]
   };
 
 }());
